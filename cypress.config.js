bed217e83d8a02134926bd23a95c8c961aa7d38d
Lucas <flucas.marquez@hotmail.com.ar>
2024-04-02 17:15:36 -0300

    Subiendo Cambios

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: true,
+  viewportWidth: 1500,
+  viewportHeight: 864,
+  chromeWebSeurity: false,
+  defaultCommandTimeout: 15000,
+  pageLoadTimeout: 19000,
+  projectId: "se1mbb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/User/cypress.config.js
+++ cypress/e2e/User/cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  viewportWidth: 1500,
+  viewportHeight: 864,
+  chromeWebSeurity: false,
+  defaultCommandTimeout: 15000,
+  pageLoadTimeout: 19000,
+  projectId: "se1mbb",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
+
+   
   },
 });

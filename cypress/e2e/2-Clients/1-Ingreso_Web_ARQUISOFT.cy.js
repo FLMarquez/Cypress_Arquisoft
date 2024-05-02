cb4a54311fd4f93c8ea207fadf63caeb98342bdd
Lucas <flucas.marquez@hotmail.com.ar>
2024-05-02 00:01:23 -0300

    Subiendo Cambios

--- cypress/e2e/2-Clients/1-Ingreso_Web_ARQUISOFT.cy.js
+++ cypress/e2e/2-Clients/1-Ingreso_Web_ARQUISOFT.cy.js
@@ -1,27 +1,20 @@
-/// <reference types='cypress' />
+/// <reference types="cypress" />
 require('cypress-xpath')
 
 
-describe('Ingresar a la Web como Cliente', () => {
-let tiempo = 1000;
-  before(() => {
-    cy.visit('https://localhost:7085/');
-    //cy.title().should('eq', 'Título');
-    cy.wait(tiempo);
-});
-  it('Ingreso Web', () => {
-    cy.get('.display-1').should('be.visible');
-    cy.wait(tiempo) 
-    cy.get(':nth-child(1) > .card > .card-body > .card-title').should('be.visible');
-    cy.wait(tiempo)
-    cy.get(':nth-child(2) > .card > .card-body > .card-title').should('be.visible');
-    cy.wait(tiempo)
-    cy.get(':nth-child(1) > .card > .card-body > .card-text').should('be.visible');
-    cy.wait(tiempo)
-    cy.get(':nth-child(2) > .card > .card-body > .card-text').should('be.visible');
-    cy.wait(tiempo)
-});
-});
+describe('PRUEBA DE LA FUNCIONALIDAD DE USUARIOS: HAPPY PATH', () => {
+    it('passes', () => {
+      //INGRESO A LA WEB DE ARQUISOFT
+      cy.visit('https://localhost:7085/')   
+      cy.get('.display-1').should("be.visible")
+      cy.get(':nth-child(1) > .card > .card-body > .card-title').should("be.visible")
+      cy.get(':nth-child(2) > .card > .card-body > .card-title').should("be.visible")
+      cy.get(':nth-child(1) > .card > .card-body > .card-text').should("be.visible")
+      cy.get(':nth-child(2) > .card > .card-body > .card-text').should("be.visible")
+
+    })
+})
+
 
   
 
